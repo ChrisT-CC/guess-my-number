a65d1884cfb504f5782c46b3823b37be8252e4be
ChrisT-CC <cristaecc@gmail.com>
2021-08-08 19:05:17 +0000

    Add event listeners to document loading and answer-box. Initialize mainGame function and generateRandomNumber function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
-function mainGame() {
 
+
+// Wait for the DOM to finish loading before running the game
+// Get the button element and add event listeners to it
+
+document.addEventListener("DOMContentLoaded", function() {
+    let guess = document.getElementById("answer-box");
+    guess.addEventListener("click", mainGame);
+})
+
+function mainGame() {
+    generateRandomNumber();
+    for (let i = 0; i < 10; i++) {
+        
+    }
+}
+
+function generateRandomNumber() {
+    // Create a random number from 1 to 100
+    let rNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(rNumber);
 }
 
 function checkGuess() {
@@ -11,5 +30,5 @@ function generateFeedback() {
 }
 
 function incrementGuess() {
-    
+
 }
