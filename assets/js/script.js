29815700383f47c812b7622586188a24f4c28a73
ChrisT-CC <cristaecc@gmail.com>
2021-08-12 09:13:51 +0000

    Add a function to check a valid number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let startButton = document.getElementById("new-game");
     startButton.addEventListener("click", startGame)
 });
 
 // Get elements form the DOM and assign them to variables
 let guessButton = document.getElementById("guess");
-let userGuess = document.getElementById("answer-box").value;
+let userGuess = document.getElementById("answer-box");
 let feedback = document.getElementById("feedback");
 let yourTry = document.getElementById("tries");
+let genNo = generateRandomNumber();
+
 
 function startGame() {
     console.log("Start game");
@@ -19,21 +21,33 @@ function startGame() {
     // checkValidNumber();
 }
 
-// /**
-//  * Generate a random number from 1 to 100
-//  * @returns rNumber
-//  */
-// function generateRandomNumber() {
-//     let rNumber = Math.floor(Math.random() * 100) + 1;
-//     return rNumber;
-// }
+/**
+ * Generate a random number from 1 to 100
+ * @returns rNumber
+ */
+function generateRandomNumber() {
+    let rNumber = Math.floor(Math.random() * 100) + 1;
+    return rNumber;
+}
 
+/**
+ * Check if a user guess is a valid number  
+ * A valid number is from 1 to 100 
+ */
 function checkValidNumber() {
-    console.log(rNumber);
     console.log("  Check Valid Number 1 - 100");
+    console.log(genNo); // debuging
+
+    // Check if entered guess is valid (from 1 to 100)
+
+    if (userGuess.value < 1 || userGuess.value > 100) {
+        alert("Please enter a number between 1 and 100.");
+
+    } else {
+        console.log("Your guess " + userGuess.value + " is valid number");
+    }
     console.log("    if yes - checkGuess");
     checkGuess();
-    console.log("    if no - alert: Number is not 1 - 100!!");
 }
 
 function checkGuess() {
@@ -58,6 +72,8 @@ function generateFeedback() {
     console.log("                  Feedback message!");
 }
 
-// add event listener for GuessButton and check if the user guess is a valid number
-guessButton.addEventListener("click", checkValidNumber);
-let rNumber = Math.floor(Math.random() * 100) + 1;
+// add event listener for guessButton and check if the user guess is a valid number
+guessButton.addEventListener("click", function () {
+    console.log("Random number when you press guessButton is: " + genNo);
+    checkValidNumber();
+})
